90373898e8168d1d99def7e531438d9adab20d93
Chris Finney <chrisfinney86@gmail.com>
2017-09-07 01:11:14 -0500

    api call fix

--- client/app/components/core/footer/footerFactory.js
+++ client/app/components/core/footer/footerFactory.js
@@ -2,5 +2,5 @@ angular.module("app.factories", ["ngResource"])
 
     .factory("GithubLimit", ["$resource", function ($resource) {
         "use strict";
-        return $resource("/api/githublimit");
+        return $resource("http://cfin-shipt-challenge.us-east-2.elasticbeanstalk.com/api/githublimit");
     }]);
